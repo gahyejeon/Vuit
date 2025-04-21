9f3a6dded64c857c081ee31271f9f0f04c48a2da
Kaia <gahyejeon@gmail.com>
2025-04-21 17:32:16 +0900

    [Design] 상세보기 화면 색 수정

--- Vuit/DetailView.swift
+++ Vuit/DetailView.swift
@@ -11,45 +11,49 @@ struct DetailView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var modelContext
     var item: Item
-
+    
     var body: some View {
-        
-        VStack(alignment: .leading, spacing: 16) {
-            // 아이템 정보 박스
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color(red: 1.0, green: 0.957, blue: 0.639))
-                .frame(height: 300)
-                .overlay(
-                    Text("Created at \(item.timestamp.formatted(date: .abbreviated, time: .shortened))")
-                        .foregroundStyle(.black)
-                        .padding()
-                )
-
-            // 버튼 두 개를 HStack으로 붙이기
-            HStack(spacing: 5) {
-                Spacer()
+        ZStack {
+            Color.backColor.ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 16) {
+                // 아이템 정보 박스
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.postColor)
+                    .frame(height: 300)
+                    .overlay(
+                        Text("Created at \(item.timestamp.formatted(date: .abbreviated, time: .shortened))")
+                            .foregroundStyle(.black)
+                            .padding()
+                    )
                 
-                Button("수정하기") {
-                    // 수정 기능
+                // 버튼 두 개를 HStack으로 붙이기
+                HStack(spacing: 5) {
+                    Spacer()
+                    
+                    Button("수정하기") {
+                        // 수정 기능
+                    }
+                    .padding()
+                    .background(Color.clear)
+                    .foregroundStyle(.gray)
+                    
+                    Button("삭제하기") {
+                        modelContext.delete(item)
+                        dismiss()
+                    }
+                    .padding()
+                    .background(Color.clear)
+                    .foregroundStyle(.gray)
                 }
-                .padding()
-                .background(Color.clear)
-                .foregroundStyle(.gray)
-
-                Button("삭제하기") {
-                    modelContext.delete(item)
-                    dismiss()
-                }
-                .padding()
-                .background(Color.clear)
-                .foregroundStyle(.gray)
+                
+                Spacer()
             }
-
-            Spacer()
+            .padding()
+            .navigationTitle("부잇 - 상세글보기")
         }
-        .padding()
-       .navigationTitle("부잇 - 상세글보기")
     }
+    
 }
 
 
